077f1c17b64a88c4fceb77d5bbb957871d05d9a0
Mawia <m.katiwa@alustudent.com>
2024-09-05 22:52:45 +0300

    python

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate
+    the floor of a floating-point number.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of the given floating-point number.
+
+    Args:
+        n (float): The floating-point number to floor.
+
+    Returns:
+        int: The floor value of the number.
+    """
+    return math.floor(n)
+    
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to convert a floating-point number
+    to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of the given floating-point number.
+
+    Args:
+        n (float): The floating-point number to convert to a string.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
+    
