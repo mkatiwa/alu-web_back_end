2fa71221952d022112f1d19e7546f837123418ad
Mawia <m.katiwa@alustudent.com>
2024-10-19 20:14:02 +0300

    4

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Flask app
 """
 from flask import Flask, render_template, request
-from os import getenv
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-
-
 class Config:
-    """ class config"""
+    """
+    Config class
+    """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.config.from_object(Config)
+
 babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """ GET /
+    Return:
+      - Render template
+    """
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """best match locale lang"""
-    local_lang = request.args.get('locale')
-    support_lang = app.config['LANGUAGES']
-    if local_lang in support_lang:
-        return local_lang
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/')
-def index():
-    """hello world"""
-    return render_template("4-index.html", message="Welcome to Holberton")
+    """
+    Get locale from request
+    """
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host="0.0.0.0", port="5000")
