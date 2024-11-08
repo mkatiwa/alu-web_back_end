14fa4b7998484bac34c5920022394aea04240a86
Mawia <m.katiwa@alustudent.com>
2024-11-08 23:50:47 +0300

    correction

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,14 +1,14 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('name must be a string');
-    if (typeof length !== 'number') throw TypeError('length must be a number');
-    if (students.constructor !== Array && students.every((el) => typeof el === 'string')) {
-        throw TypeError('students must be an array of strings');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
-    }
+constructor(name, length, students) {
+if (typeof name !== 'string') throw TypeError('name must be a string');
+if (typeof length !== 'number') throw TypeError('length must be a number');
+if (students.constructor !== Array && students.every((el) => typeof el === 'string')) {
+throw TypeError('students must be an array of strings');
+}
+this._name = name;
+this._length = length;
+this._students = students;
+}
 
     get name() {
     return this._name;
