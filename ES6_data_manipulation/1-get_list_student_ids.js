7e33d1f0f4bd270809d4e2be3f4a08defb793e8e
Mawia <m.katiwa@alustudent.com>
2024-11-09 14:39:53 +0300

    hjsr

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(arrayOfObjects) {
-    const arr = [];
-    if (Array.isArray(arrayOfObjects)) {
-      return arrayOfObjects.map((item) => item.id);
-    }
-    return arr;
+  const arr = [];
+  if (Array.isArray(arrayOfObjects)) {
+    return arrayOfObjects.map((item) => item.id);
   }
+  return arr;
+}
