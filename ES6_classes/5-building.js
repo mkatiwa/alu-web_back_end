bffed93ae357e4a6e6bac7f559a9d1f04b40ed12
Mawia <m.katiwa@alustudent.com>
2024-11-09 15:24:41 +0300

    5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,25 +1,22 @@
 export default class Building {
   constructor(sqft) {
   this.sqft = sqft;
-
-    if (
-      this.constructor !== Building
-      && this.evacuationWarningMessage === undefined
-    ) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
-    }
-  }
-
+  if (
+    this.constructor !== Building
+    && this.evacuationWarningMessage === undefined
+  ) {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
+}
+}
   get sqft() {
-    return this._sqft;
+  return this._sqft;
+}
+set sqft(sqft) {
+  if (typeof sqft !== 'number') {
+    throw new TypeError('Sqft must be a number');
   }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    this._sqft = sqft;
-  }
+  this._sqft = sqft;
+}
 }
